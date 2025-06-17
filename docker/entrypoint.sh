3fe08ccba45841c7ab5a1306537cb2fd030ffcb9
Tig Campbell-Moore <mcmoore@gmail.com>
2025-06-17 02:01:50 +0100

    Failure number 1002023123

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ echo "🔧 Configuring cron schedule..."
 export CRON_SCHEDULE="${CRON_SCHEDULE:-*/3 * * * *}"
 
 # Inject schedule into template
-envsubst < docker/crontab.template > /etc/cron.d/dayzmonitor
+envsubst < /tmp/crontab.template > /etc/cron.d/dayzmonitor
 chmod 0644 /etc/cron.d/dayzmonitor
 
 echo "⏰ Starting cron daemon with schedule: $CRON_SCHEDULE"
